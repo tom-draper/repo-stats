0b241caaaebf4eb279c10db1de886579af404f05
Tom Draper <tomjdraper1@gmail.com>
2022-11-16 00:02:39 +0000

    Memory

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ struct FileCounts {
 #[derive(Debug)]
 pub struct Stats {
     files: FileCounts,
+    memory: FileCounts,
     lines: CodeCounts,
     chars: CodeCounts,
 }
@@ -79,6 +80,12 @@ fn main() {
             binaries: 0,
             storage: 0,
         },
+        memory: FileCounts {
+            total: 0,
+            code: 0,
+            binaries: 0,
+            storage: 0,
+        },
         lines: CodeCounts {
             total: 0,
             code: 0,
@@ -91,47 +98,47 @@ fn main() {
         },
     };
 
-    let ignore_dir = HashSet::from(["target", ".vscode"]);
-    let target_dir = HashSet::from(["src"]);
+    let ignore_dir = HashSet::from([]);
+    let target_dir = HashSet::from([]);
 
     let source_code_exts = extensions::source_code_extensions();
     let binary_exts = extensions::binary_extensions();
     let storage_exts = extensions::storage_extensions();
 
     for f in WalkDir::new(".").into_iter().filter_map(|f| f.ok()) {
-        if f.metadata().unwrap().is_file() {
+        let metadata = f.metadata().unwrap();
+        if metadata.is_file() {
             let path = f.path();
-
             stats.files.total += 1;
+            stats.memory.total += metadata.len(); 
 
-            println!("{:?}", path);
+            // println!("{:?}", path);
             if is_target_file(path, &target_dir, &ignore_dir) {
                 let file_contents = fs::read_to_string(path);
                 if file_contents.is_ok() {
                     let code = file_contents.unwrap();
+                    // Add one line for final line
+                    let lines = (code.matches("\n").count() + 1) as u64;
+                    let length = code.len() as u64;
+                    stats.lines.total += lines;
+                    stats.chars.total += length;
                     if path.extension().is_some() {
                         let ext = path.extension().unwrap().to_str().unwrap();
                         if source_code_exts.contains(ext) {
                             stats.files.code += 1;
-                            let lines = (code.matches("\n").count() + 1) as u64;
                             stats.lines.code += lines;
-                            let length = code.len() as u64;
                             stats.chars.code += length;
+                            stats.memory.code += metadata.len(); 
                         } else if storage_exts.contains(ext) {
                             stats.files.storage += 1;
-                            let lines = (code.matches("\n").count() + 1) as u64;
                             stats.lines.storage += lines;
-                            let length = code.len() as u64;
                             stats.chars.storage += length;
+                            stats.memory.storage += metadata.len(); 
                         } else if binary_exts.contains(ext) {
                             stats.files.binaries += 1;
+                            stats.memory.binaries += metadata.len(); 
                         }
                     }
-                    // Add one line for final line
-                    let lines = (code.matches("\n").count() + 1) as u64;
-                    stats.lines.total += lines;
-                    let length = code.len() as u64;
-                    stats.chars.total += length;
                 }
             }
         }
--- src/view.rs
+++ src/view.rs
@@ -2,20 +2,26 @@ use crate::Stats;
 use num_format::{Locale, ToFormattedString};
 
 pub fn display_stats(stats: Stats) {
-    println!("Total files: {}", stats.files.total.to_formatted_string(&Locale::en));
-    println!("Total lines: {}", stats.lines.total.to_formatted_string(&Locale::en));
-    println!("Total characters: {}", stats.chars.total.to_formatted_string(&Locale::en));
-    println!("Total average line length: {}\n", stats.avg_total_line_len().to_formatted_string(&Locale::en));
+    println!("Total");
+    println!("Files: {}", stats.files.total.to_formatted_string(&Locale::en));
+    println!("Lines: {}", stats.lines.total.to_formatted_string(&Locale::en));
+    println!("Characters: {}", stats.chars.total.to_formatted_string(&Locale::en));
+    println!("Average line length: {}", stats.avg_total_line_len().to_formatted_string(&Locale::en));
+    println!("Memory: {} KB\n", (stats.memory.total/1024).to_formatted_string(&Locale::en));
 
-    println!("Source code files: {}", stats.files.code.to_formatted_string(&Locale::en));
-    println!("Source code lines: {}", stats.lines.code.to_formatted_string(&Locale::en));
-    println!("Source code characters: {}", stats.chars.code.to_formatted_string(&Locale::en));
-    println!("Source code average line length: {}\n", stats.avg_code_line_len().to_formatted_string(&Locale::en));
+    println!("Source code");
+    println!("Files: {}", stats.files.code.to_formatted_string(&Locale::en));
+    println!("Lines: {}", stats.lines.code.to_formatted_string(&Locale::en));
+    println!("Characters: {}", stats.chars.code.to_formatted_string(&Locale::en));
+    println!("Average line length: {}", stats.avg_code_line_len().to_formatted_string(&Locale::en));
+    println!("Memory: {} KB\n", (stats.memory.code/1024).to_formatted_string(&Locale::en));
 
-    println!("Storage files: {}", stats.files.storage.to_formatted_string(&Locale::en));
-    println!("Storage lines: {}", stats.lines.storage.to_formatted_string(&Locale::en));
-    println!("Storage characters: {}", stats.chars.storage.to_formatted_string(&Locale::en));
-    println!("Storage average line length: {}\n", stats.avg_storage_line_len().to_formatted_string(&Locale::en));
+    println!("Storage");
+    println!("Files: {}", stats.files.storage.to_formatted_string(&Locale::en));
+    println!("Lines: {}", stats.lines.storage.to_formatted_string(&Locale::en));
+    println!("Characters: {}", stats.chars.storage.to_formatted_string(&Locale::en));
+    println!("Average line length: {}", stats.avg_storage_line_len().to_formatted_string(&Locale::en));
+    println!("Memory: {} KB\n", (stats.memory.storage/1024).to_formatted_string(&Locale::en));
 
-    println!("Binaries {}", stats.files.binaries.to_formatted_string(&Locale::en));
+    println!("Binaries: {}", stats.files.binaries.to_formatted_string(&Locale::en));
 }
